2ceb9cd92f80c776059408a67f7027d218f5f328
Aleksandr <sashabogush@gmail.com>
2020-11-15 17:54:23 +0200

    add ServicePerson

--- src/main/java/com/ua/sasha/bogush/springbootmicrocervices/SpringbootAlfrescoMicrocervicesApplication.java
+++ src/main/java/com/ua/sasha/bogush/springbootmicrocervices/SpringbootAlfrescoMicrocervicesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootAlfrescoMicrocervicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAlfrescoMicrocervicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAlfrescoMicrocervicesApplication.class, args);
+    }
 
 }
